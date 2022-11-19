7b3aaf5cbc3caf44189b543de897cf4df5c30699
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-19 17:06:10 -0300

    #3 - lista de herois criada e exibida na pagina inicial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./heroes.model";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Homem-Aranha'},
+    { id: 2, name: 'Homem de Ferro'},
+    { id: 3, name: 'Superman'},
+    { id: 4, name: 'Capitao America'},
+    { id: 5, name: 'Homem Formiga'},
+    { id: 6, name: 'Pantera Negra'},
+    { id: 7, name: 'Homem-Aranha'},
+    { id: 8, name: 'Homem-Aranha'},
+    { id: 9, name: 'Homem-Aranha'},
+    { id: 10, name: 'Homem-Aranha'},
+]
