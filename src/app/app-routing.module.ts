e813ee1e68ee966636519a376bacd0fadbfc5eb1
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-20 14:27:54 -0300

    #13 - Dashboard foi associado a um routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'heroes', component: HeroesComponent}
 ];
 
