8fa79578614a565b1d26514689e493b59d1b5bed
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-20 12:19:17 -0300

    #11 - criado os metodos add() e clear() no MessageService

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  messages: string[] = [];
 
   constructor() { }
+
+  add(messages: string): void {
+    this.messages.push(messages);
+  };
+
+  clear(): void {
+    this.messages = [];
+  }
 }
