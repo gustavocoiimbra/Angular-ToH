a69f5bacf3a488984279c8fe4fd1792cd210d69a
Gustavo Coimbra <gustavocoimbra@discente.ufg.br>
2022-11-20 17:06:22 -0300

    #15 - HeroService refeito para suportar uma API web

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,25 +3,35 @@ import { Observable, of } from 'rxjs';
 import {HEROES} from './mock-heroes';
 import { Hero } from './heroes.model';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
+  private heroUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, 
+    private htpp: HttpClient) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.htpp.get<Hero[]>(this.heroUrl);
+
+
+    // const heroes = of(HEROES);
+    // this.log('fetched heroes');
+    // return heroes;
   }
 
   getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(hero => hero.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero)
+    this.log(`fetched hero id=${id}`);
+    return of(hero);
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroesService: ${message}`)
   }
 
 }
